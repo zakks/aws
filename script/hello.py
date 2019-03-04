15b4a4bbcc5080dc938267fc79e7578a8b184405
Zakir <zsheikh@egnyte.com>
2019-03-05 00:07:24 +0530

    new hello python script uploaded

--- script/hello.py
+++ script/hello.py
@@ -1,28 +1,35 @@
 import boto3, json
-client = boto3.client('lambda', region_name='ap-south-1', aws_access_key_id='AKIAI3ZYDANHF7TA6AYA', aws_secret_access_key='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')
+client = boto3.client('lambda', region_name='ap-south-1', aws_access_key_id='AKIAJZGDDMAH5FJ6U5IQ', aws_secret_access_key='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')
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+## function to display hello world current time
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route("/uc/<input>", methods=['GET'])
-def change_uppercase(input):
-    payload = '{"key":"%s"}' % (input)
-    response = client.invoke(FunctionName='helloworld', Payload=payload)
-    print(response)
-
-    output= json.loads(response['Payload'].read().decode('utf-8'))
-    result1 = {'input' :input ,'return_str':output}
-    return render_template('lowertouppercase.html', result=result1)
+## function to display login page
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+## function to display string in uppercase from lambda function
+
+@app.route("/uc/<param>", methods=['GET'])
+def helloworld(param):
+    payload = '{"key":"%s"}' % (param)
+    response = client.invoke(FunctionName='helloworld', Payload=payload)
+    print(response)
+
+    output= json.loads(response['Payload'].read().decode('utf-8'))
+    result1 = {'input' :input ,'return_str':output}
+    return render_template('upper.html', result=result1)
+
 
 app.run(host='0.0.0.0', port=8888)
+
